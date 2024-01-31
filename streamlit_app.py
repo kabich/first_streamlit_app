e0cc9f3ed9adb150af15c1c96cadf89b123973db
kabich <74920065+kabich@users.noreply.github.com>
2024-01-31 11:05:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ df  = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_m
 streamlit.dataframe(df)
 
 
-streamlit.multiselect('pick some fruits', df.index)
+streamlit.multiselect('pick some fruits', df['Fruit'].unique())
