8f9c89b3f1b35defff56e5521821664b1db9cdef
kabich <74920065+kabich@users.noreply.github.com>
2024-01-31 11:19:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 df  = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-streamlit.dataframe(df)
 
 
-streamlit.multiselect('pick some fruits', df['Fruit'].unique(), ['Avocado','Strawberries'])
+
+data_selected = streamlit.multiselect('pick some fruits', df['Fruit'].unique(), ['Avocado','Strawberries'])
+
+streamlit.dataframe(df.loc[df['Fruit'].isin(data_selected)])
