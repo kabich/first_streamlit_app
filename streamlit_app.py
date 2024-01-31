4017c8913d167aff97f929ada15b039823a88496
kabich <74920065+kabich@users.noreply.github.com>
2024-01-31 10:59:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit
 import pandas as pd
-"""
-streamlit.title('hello world 🥣 🥗 🐔 🥑🍞')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')"""
 
 df  = read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
