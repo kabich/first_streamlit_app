d1337fc17f70d2ed18f52c9fca54d67096f341df
kabich <74920065+kabich@users.noreply.github.com>
2024-01-31 11:00:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas as pd
 
-df  = read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+df  = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 streamlit.dataframe(df)
