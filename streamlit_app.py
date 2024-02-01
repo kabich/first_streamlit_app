2928ae550764903de737937263117db1a2741ae6
kabich <74920065+kabich@users.noreply.github.com>
2024-02-01 22:46:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,17 @@ streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','fruit')
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute('select * from pc_rivery_db.public.fruit_load_list')
+    return my_cur.fetchall()
+
+if streamlit.button('get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  data = get_fruit_load_list()
+  streamlit.dataframe(data)
+
+
+
