aabe69a86fce81badef61a7973d7d334cbaeaaa8
kabich <74920065+kabich@users.noreply.github.com>
2024-02-01 22:13:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,4 @@ my_data_row = my_cur.fetchall()
 streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','fruit')
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
